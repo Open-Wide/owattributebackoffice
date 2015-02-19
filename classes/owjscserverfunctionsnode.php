e9b48160751c18fd68a3a9d84591dfb3038bd231
ajrad <anis.jrad@openwide.fr>
2015-02-19 15:46:14 +0100

    add show attribute in back-office

---
+++ classes/owjscserverfunctionsnode.php
@@ -0,0 +1,123 @@
+<?php
+//
+// Definition of ezjscServerFunctionsNode class
+//
+// Created on: <01-Jun-2010 00:00:00 ls>
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: eZ JSCore extension for eZ Publish
+// SOFTWARE RELEASE: 1.x
+// COPYRIGHT NOTICE: Copyright (C) 1999-2014 eZ Systems AS
+// SOFTWARE LICENSE: GNU General Public License v2.0
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of version 2.0  of the GNU General
+//   Public License as published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
+
+/**
+ * owjscServerFunctionsNode class definition that provide node fetch functions
+ *
+ */
+class owjscServerFunctionsNode extends ezjscServerFunctionsNode
+{
+    /**
+     * Returns a subtree node items for given parent node
+     *
+     * Following parameters are supported:
+     * ezjscnode::subtree::parent_node_id::limit::offset::sort::order
+     *
+     * @since 1.2
+     * @param mixed $args
+     * @return array
+     */
+    public static function subTree( $args )
+    {
+        $parentNodeID = isset( $args[0] ) ? $args[0] : null;
+        $limit = isset( $args[1] ) ? $args[1] : 25;
+        $offset = isset( $args[2] ) ? $args[2] : 0;
+        $sort = isset( $args[3] ) ? self::sortMap( $args[3] ) : 'published';
+        $order = isset( $args[4] ) ? $args[4] : false;
+        $objectNameFilter = isset( $args[5] ) ? $args[5] : '';
+        if(preg_match('/data_map./', $sort)){
+            $sort = str_replace('data_map.', '', $sort);
+            $sort = array( array( 'attribute', $order, $args[6].'/'.$sort ) );
+        } else {
+            $sort = array( array( $sort, $order ) );
+        }
+
+        if ( !$parentNodeID )
+        {
+            throw new ezcBaseFunctionalityNotSupportedException( 'Fetch node list', 'Parent node id is not valid' );
+        }
+
+        $node = eZContentObjectTreeNode::fetch( $parentNodeID );
+        if ( !$node instanceOf eZContentObjectTreeNode )
+        {
+            throw new ezcBaseFunctionalityNotSupportedException( 'Fetch node list', "Parent node '$parentNodeID' is not valid" );
+        }
+
+
+        $params = array( 'Depth' => 1,
+                         'Limit' => $limit,
+                         'Offset' => $offset,
+                         'SortBy' => $sort,
+                         'DepthOperator' => 'eq',
+                         'ObjectNameFilter' => $objectNameFilter,
+                         'AsObject' => true );
+
+       // fetch nodes and total node count
+        $count = $node->subTreeCount( $params );
+        if ( $count )
+        {
+            $nodeArray = $node->subTree( $params );
+        }
+        else
+        {
+            $nodeArray = array();
+        }
+
+        unset( $node );// We have on purpose not checked permission on $node itself, so it should not be used
+
+        // generate json response from node list
+        if ( $nodeArray )
+        {
+            $ini = eZINI::instance( 'owattributebackoffice.ini' );
+            $attribute = $ini->variable( 'AttributeColumns', 'Attribute' );
+            $list = ezjscAjaxContent::nodeEncode( $nodeArray, array( 'formatDate' => 'shortdatetime',
+                                                                     'fetchThumbPreview' => true,
+                                                                     'fetchSection' => true,
+                                                                     'fetchCreator' => true,
+                                                                     'fetchClassIcon' => true,
+                                                                     'dataMap' => $attribute), 'raw' );
+        }
+        else
+        {
+            $list = array();
+        }
+
+        return array( 'parent_node_id' => $parentNodeID,
+                      'count' => count( $nodeArray ),
+                      'total_count' => (int)$count,
+                      'list' => $list,
+                      'limit' => $limit,
+                      'offset' => $offset,
+                      'sort' => $sort,
+                      'order' => $order );
+    }
+}
+
+?>
---
+++ settings/ezjscore.ini.append.php
@@ -0,0 +1,11 @@
+<?php /* #?ini charset="utf-8"? */
+
+/*
+
+[ezjscServer_ezjscnode]
+# Url to test this server function(return node list):
+# <root>/ezjscore/call/ezjscnode::subtree::2
+Class=owjscServerFunctionsNode
+#File=extension/owbackoffice/classes/owjscserverfunctionsnode.php
+
+*/
