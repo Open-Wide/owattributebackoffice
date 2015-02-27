5b1902cbd8784e60b3a3e2b91e583e930ae67f88
ajrad <anis.jrad@openwide.fr>
2015-02-27 12:15:43 +0100

    add handler for attribute type  ezobjectrelationlist

---
+++ attribute_handler/ezobjectrelationlistattributehandler.php
@@ -0,0 +1,24 @@
+<?php
+
+class ezobjectrelationlistAttributeHandler extends DefaultDatatypeAttributeHandler {
+
+    static public function toString( eZContentObjectAttribute $attribute ) {
+        $result = '';
+        if($attribute->hasContent()) {
+            $data = array();
+            $content = $attribute->content();
+            foreach($content['relation_list'] as $relation) {
+                $node = eZFunctionHandler::execute('content', 'node', array(
+                    'node_id' => $relation['node_id']
+                ));
+                if($node instanceof eZContentObjectTreeNode) {
+                    array_push($data, $node->getName());
+                }
+            }
+            $result = implode(' , ', $data);
+            unset($content, $data);
+        }
+        return $result;
+    }
+}
+?>
